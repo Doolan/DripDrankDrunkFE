709c49526444dbb8153ba0cf849377a670af52de
Steven Doolan <doolan@rose-hulman.edu>
2017-01-20 23:00:27 -0500

    template

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dripdrankdrunkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dripdrankdrunkApp
+ */
+angular.module('dripdrankdrunkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
