1c0bfee676094fcfb2da3816569ff7db4ff26156
Ben Brubaker <heavensords1@gmail.com>
2017-01-22 03:50:04 -0500

    Registration fully functional. Login screen split into login/registration.

--- src/js/controllers/login.js
+++ src/js/controllers/login.js
@@ -8,9 +8,12 @@
  * Controller of the dripdrankdrunkApp
  */
 angular.module('dripdrankdrunkApp')
-  .controller('LoginCtrl', ['$scope', 'AuthService', '$state', function ($scope, AuthService, $state) {
+  .controller('LoginCtrl', ['$scope', 'AuthService', '$state','DataService', function ($scope, AuthService, $state,DataService) {
     var loginPageSetup = function () {
-      $('.ui.form')
+      $('.menu .item')
+        .tab()
+        ;
+      $('#login')
         .form({
           fields: {
             username: {
@@ -39,6 +42,59 @@ angular.module('dripdrankdrunkApp')
               event.preventDefault();
             }
             authenticate(fields);
+            return false;
+
+          },
+          onFailure: function (formErrors, fields) {
+            return; // What happens when the form is not filed out
+          }
+        });
+      $('#registration')
+        .form({
+          fields: {
+            username: {
+              identifier: 'regusername',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter your username'
+                }
+              ]
+            },
+            password: {
+              identifier: 'regpassword',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter your password'
+                },
+                {
+                  type: 'match[regpassword2]',
+                  prompt: 'Your passwords must match'
+                }
+              ]
+            },
+            password: {
+              identifier: 'regpassword2',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter your password'
+                },
+                {
+                  type: 'match[regpassword]',
+                  prompt: 'Your passwords must match'
+                }
+              ]
+            }
+          },
+          inline: true,
+          onSuccess: function (event, fields) {
+            //what happens when the form is filed in            
+            if (event) {
+              event.preventDefault();
+            }
+            DataService.newUser(fields.regusername,fields.regpassword);
             return false;
 
           },
