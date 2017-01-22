342f90c72821fbec9704322530a37372cdfe5e15
Ben Brubaker <heavensords1@gmail.com>
2017-01-22 05:22:24 -0500

    Not quite there

--- src/js/controllers/login.js
+++ src/js/controllers/login.js
@@ -8,11 +8,10 @@
  * Controller of the dripdrankdrunkApp
  */
 angular.module('dripdrankdrunkApp')
-  .controller('LoginCtrl', ['$scope', 'AuthService', '$state','DataService', function ($scope, AuthService, $state,DataService) {
+  .controller('LoginCtrl', ['$scope', 'AuthService', '$state', 'DataService', function ($scope, AuthService, $state, DataService) {
+
     var loginPageSetup = function () {
-      $('.menu .item')
-        .tab()
-        ;
+
       $('#login')
         .form({
           fields: {
@@ -36,78 +35,33 @@ angular.module('dripdrankdrunkApp')
             }
           },
           inline: true,
-          onSuccess: function (event, fields) {
-            //what happens when the form is filed in            
+          onSuccess: function (event,fields) {
             if (event) {
               event.preventDefault();
             }
-            authenticate(fields);
             return false;
-
           },
           onFailure: function (formErrors, fields) {
-            return; // What happens when the form is not filed out
+            return;
+            
           }
-        });
-      $('#registration')
-        .form({
-          fields: {
-            username: {
-              identifier: 'regusername',
-              rules: [
-                {
-                  type: 'empty',
-                  prompt: 'Please enter your username'
-                }
-              ]
-            },
-            password: {
-              identifier: 'regpassword',
-              rules: [
-                {
-                  type: 'empty',
-                  prompt: 'Please enter your password'
-                },
-                {
-                  type: 'match[regpassword2]',
-                  prompt: 'Your passwords must match'
-                }
-              ]
-            },
-            password: {
-              identifier: 'regpassword2',
-              rules: [
-                {
-                  type: 'empty',
-                  prompt: 'Please enter your password'
-                },
-                {
-                  type: 'match[regpassword]',
-                  prompt: 'Your passwords must match'
-                }
-              ]
-            }
-          },
-          inline: true,
-          onSuccess: function (event, fields) {
-            //what happens when the form is filed in            
-            if (event) {
-              event.preventDefault();
-            }
-            DataService.newUser(fields.regusername,fields.regpassword);
-            return false;
 
-          },
-          onFailure: function (formErrors, fields) {
-            return; // What happens when the form is not filed out
-          }
         });
     };
+
+    $scope.loginFunc = function () {
+      $('#login').validate();
+      authenticate(fields);
+    }
+    $scope.registerFunc = function () {
+      DataService.newUser(fields.regusername, fields.regpassword);
+    }
+
 
     //on scope load
     $scope.$on('$viewContentLoaded', function () {
       loginPageSetup();
-    });
+    })
 
 
 
@@ -124,5 +78,5 @@ angular.module('dripdrankdrunkApp')
           $state.go('user.history');
         }
       });
-    };
+    }
   }]);
